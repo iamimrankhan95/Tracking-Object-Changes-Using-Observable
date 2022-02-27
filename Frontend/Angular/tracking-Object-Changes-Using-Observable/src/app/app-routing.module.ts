ff25352fc7bbf3aeb8080ad07bd54628b57668d0
jannat ruma <jannatruma@hotmail.com>
2022-02-27 12:55:36 +0600

    Made Routing for Scene1/Component1 (Login UI)

--- Frontend/Angular/tracking-Object-Changes-Using-Observable/src/app/app-routing.module.ts
+++ Frontend/Angular/tracking-Object-Changes-Using-Observable/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'authentication', loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule) },
-  { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) }
+  { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
+  { path: 'sign-in', loadChildren: () => import('./modules/authentication/sign-in/sign-in.module').then(m => m.SignInModule) }
 ];
 
 @NgModule({
