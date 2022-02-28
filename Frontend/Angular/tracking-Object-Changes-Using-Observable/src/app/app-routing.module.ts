4b30f519dfd31d0f4d8f34739802fd21b4b6680f
imran-khan4 <imran.khan4@bd.ey.com>
2022-02-28 08:18:05 +0600

    refact(angular): delete unnecessay style from appModule

--- Frontend/Angular/tracking-Object-Changes-Using-Observable/src/app/app-routing.module.ts
+++ Frontend/Angular/tracking-Object-Changes-Using-Observable/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'user' },
   { path: 'authentication', loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule) },
   { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) }
 ];
