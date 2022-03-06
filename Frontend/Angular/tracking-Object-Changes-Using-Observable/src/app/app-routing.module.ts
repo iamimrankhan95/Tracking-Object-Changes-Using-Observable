301eaa9dd8d25884f4c6d95d0f11101ba279f169
imran-khan4 <imran.khan4@bd.ey.com>
2022-03-06 16:32:33 +0600

    fix(angualr): fix authentication routing

--- Frontend/Angular/tracking-Object-Changes-Using-Observable/src/app/app-routing.module.ts
+++ Frontend/Angular/tracking-Object-Changes-Using-Observable/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'authentication', loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule) },
   { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
-  { path: 'sign-in', loadChildren: () => import('./modules/authentication/sign-in/sign-in.module').then(m => m.SignInModule) }
 ];
 
 @NgModule({
--- Frontend/Angular/tracking-Object-Changes-Using-Observable/src/app/modules/authentication/authentication-routing.module.ts
+++ Frontend/Angular/tracking-Object-Changes-Using-Observable/src/app/modules/authentication/authentication-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [{ path: '', component: AuthenticationComponent }];
+const routes: Routes = [{
+  path: '', component: AuthenticationComponent, children: [
+    {
+      path: '', component: SignInComponent, children: []
+    }]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
